a9e5654ba32cde933021e7a0ab3e7933f1e7e86d
Raghab Neupane <neupaneraghab@gmail.com>
2025-12-25 14:51:39 +0545

    changed the UI

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,16 +1,27 @@
+import videoSrc from "../assets/trimmed.mp4";
+
 const Hero: React.FC = () => {
   return (
     <main className="hero">
-      <h1>
-        Transforming Healthcare <br />
-        Through <span>AI Innovation</span>
-      </h1>
+
+
+      <video className="hero-video" src={videoSrc} autoPlay muted loop playsInline></video>
+
+      <div className="hero-overlay"></div>
+
+      <div className="hero-content">
+        <h1>
+          Transforming Healthcare <br />
+          Through <span>AI Innovation</span>
+        </h1>
 
       <p>
         Pioneering intelligent solutions that bridge artificial intelligence
         and medical practice. From automated documentation to diagnostic
         support, we're revolutionizing healthcare delivery.
       </p>
+
+      </div>
     </main>
   );
 };
