8d78a3ac85bb551b87947c74464be6106e0b332a
Raghab Neupane <neupaneraghab@gmail.com>
2025-12-25 13:42:44 +0545

    a

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,19 +1,19 @@
 import React from "react";
-import logo from "../assets/nav-bar.svg"; // update path if needed
+import logo from "../assets/logo.png"; // update path if needed
 
 const Navbar = () => {
   return (
-    <nav>
-        <div>
+    <nav className="navbar">
+        <div className="logo">
         <img src={logo} alt="AI Lab Logo" />
         </div>
         <div>
           <ul className="Links">
-            <li>Services</li>
-            <li>Our Product</li>
-            <li>About Us</li>
-            <li>Team</li>
-            <li>Contact Us</li>
+            <li> <a href="#services">Services</a></li>
+            <li> <a href="#our-product">Our Product</a></li>
+            <li> <a href="#about-us">About Us</a></li>
+            <li> <a href="#team">Team</a></li>
+            <li> <a href="#contact-us">Contact Us</a></li>
           </ul>
         </div>
     </nav>
