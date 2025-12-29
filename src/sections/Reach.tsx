862347ed38558078b1e1e2ba485c520bc6f2d736
Raghab Neupane <neupaneraghab@gmail.com>
2025-12-29 19:43:11 +0545

    Change

--- src/sections/Reach.tsx
+++ src/sections/Reach.tsx
@@ -1,7 +1,15 @@
 
 const Reach = () => {
   return (
-    <div className='Reach'>Our Reach in <span>Nepal</span></div>
+    <div className="Reach-page">
+      <h2>Our Reach in <span>Nepal</span></h2>
+      
+      <div className='Reach-content'>
+        <p>
+          We have a strong presence in Nepal, with numerous projects and initiatives aimed at improving the lives of its citizens.
+        </p>
+      </div>
+    </div>
   )
 }
 
