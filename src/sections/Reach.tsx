6081a63348eae39b85da248eb3e017d6bbc41a52
Raghab Neupane <neupaneraghab@gmail.com>
2025-12-29 21:26:25 +0545

    Changes in ui

--- src/sections/Reach.tsx
+++ src/sections/Reach.tsx
@@ -1,13 +1,16 @@
+import React from 'react'
+import nepal from "../assets/nepal.png";
 
 const Reach = () => {
   return (
     <div className="Reach-page">
       <h2>Our Reach in <span>Nepal</span></h2>
-      
+
       <div className='Reach-content'>
         <p>
           We have a strong presence in Nepal, with numerous projects and initiatives aimed at improving the lives of its citizens.
         </p>
+        <img className='mapofnepal' src={nepal} alt="Nepal Map" />
       </div>
     </div>
   )
