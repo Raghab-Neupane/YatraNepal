ce6587feccdbb05cdfacf10a39ad17c7bbcde48f
Raghab Neupane <neupaneraghab@gmail.com>
2025-12-31 16:55:05 +0545

    deployment in vercel

--- src/sections/Banner.tsx
+++ src/sections/Banner.tsx
@@ -2,7 +2,7 @@ import ImageSlider from "../components/imgsld/imgsld1.tsx";
 import img1 from "../assets/banner.jpg";
 import img2 from "../assets/rara.jpg";
 import img3 from "../assets/town.jpg";
-import { DiVim } from "react-icons/di";
+
 
 
 
