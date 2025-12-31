32ce940b6d1a724b027ffa847179fcfce820aeee
Raghab Neupane <neupaneraghab@gmail.com>
2025-12-31 15:35:08 +0545

    change

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,7 @@
+import { useEffect } from "react";
+import AOS from "aos";
+import "aos/dist/aos.css";
+
 import Navbar from "./components/Navbar.tsx";
 import Hero from "./sections/Hero.tsx";
 import "./App.css";
@@ -6,19 +10,24 @@ import Footer from "./sections/Footer.tsx";
 import Imgbrd from "./sections/Imgbrd.tsx";
 import Hotels from "./sections/Hotels.tsx";
 
-
-
 const App: React.FC = () => {
+  useEffect(() => {
+    AOS.init({
+      duration: 900,
+      easing: "ease-out-cubic",
+      once: true,
+    });
+  }, []);
+
   return (
     <>
       <Navbar />
-  <Hero />
-  <Package />
-  <Imgbrd />
-  <Hotels />
-  
-  <Footer />
-</>
+      <Hero />
+      <Package />
+      <Imgbrd />
+      <Hotels />
+      <Footer />
+    </>
   );
 };
 
